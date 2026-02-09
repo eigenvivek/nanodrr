9456464330c2503e1054a133a0846f50f29ee8d7
eigenvivek <vivekg@mit.edu>
2026-02-09 13:07:52 -0500

    Add Double Geodesic implementation

---
+++ src/nanodrr/metrics/geo.py
@@ -0,0 +1,44 @@
+import torch
+from jaxtyping import Float
+from torch import Tensor
+
+from ..geometry import so3_log_map
+
+
+class DoubleGeodesicSE3(torch.nn.Module):
+    """Calculate the angular and translational geodesics between two SE(3) transformation matrices."""
+
+    def __init__(
+        self,
+        sdd: float,
+        eps: float = 1e-4,
+    ):
+        super().__init__()
+        self.sdr = sdd / 2
+        self.eps = eps
+
+    def forward(
+        self,
+        pose_1: Float[Tensor, "B 4 4"],
+        pose_2: Float[Tensor, "B 4 4"],
+    ) -> tuple[Float[Tensor, "B"], Float[Tensor, "B"], Float[Tensor, "B"]]:
+        r1, t1 = pose_1[:, :3, :3], pose_1[:, :3, 3]
+        r2, t2 = pose_2[:, :3, :3], pose_2[:, :3, 3]
+        rot = self._rot_geodesic(r1, r2)
+        xyz = self._xyz_geodesic(t1, t2)
+        dou = (rot.square() + xyz.square() + self.eps).sqrt()
+        return rot, xyz, dou
+
+    def _rot_geodesic(
+        self,
+        r1: Float[Tensor, "B 3 3"],
+        r2: Float[Tensor, "B 3 3"],
+    ) -> Float[Tensor, "B"]:
+        return self.sdr * so3_log_map(r1.transpose(-1, -2) @ r2).norm(dim=-1)
+
+    def _xyz_geodesic(
+        self,
+        t1: Float[Tensor, "B 3"],
+        t2: Float[Tensor, "B 3"],
+    ) -> Float[Tensor, "B"]:
+        return (t1 - t2).norm(dim=-1)
